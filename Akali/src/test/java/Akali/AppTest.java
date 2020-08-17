8d4d9ef65949a4cf72e1afa5f2f72b4f98894d36
daonq2001 <daonq.nde19046@vtc.edu.vn>
2020-08-17 08:42:15 +0700

    Viet test dang nhap

--- Akali/src/test/java/Akali/AppTest.java
+++ Akali/src/test/java/Akali/AppTest.java
@@ -2,19 +2,24 @@ package Akali;
 
 import static org.junit.Assert.assertTrue;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
+
+import BusinessLogic.CustomerBL;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
+    CustomerBL customerBL = new CustomerBL();
+
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldAnswerWithTrue() throws SQLException
     {
-        assertTrue( true );
+        assertTrue(customerBL.Login("ngoquangdao@gmail.com", "ngoquangdao"));
     }
 }
